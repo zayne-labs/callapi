2d29c307210542eea686e6f6ae39a677d27b9902
Ryan-Zayne <phoenixastro97@gmail.com>
2025-09-01 03:27:45 +0100

    refactor(docs): update tsconfig and improve mdx components typing
    - Add mdx configuration section to tsconfig
    - Simplify getMDXComponents return type with satisfies operator
    - Add global MDXProvidedComponents type declaration

--- apps/docs/components/common/MdxComponents.tsx
+++ apps/docs/components/common/MdxComponents.tsx
@@ -5,7 +5,7 @@ import * as TypeTable from "fumadocs-ui/components/type-table";
 import defaultComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
-export const getMDXComponents = (extraComponents?: MDXComponents): MDXComponents => {
+export const getMDXComponents = (extraComponents?: MDXComponents) => {
 	return {
 		...defaultComponents,
 		...Twoslash,
@@ -13,5 +13,20 @@ export const getMDXComponents = (extraComponents?: MDXComponents): MDXComponents
 		...TypeTable,
 		Link,
 		...extraComponents,
-	};
+	} satisfies MDXComponents;
 };
+
+// declare module "mdx/types.js" {
+// 	// eslint-disable-next-line ts-eslint/no-namespace
+// 	namespace JSX {
+// 		type Element = React.JSX.Element;
+// 		type ElementClass = React.JSX.ElementClass;
+// 		type ElementAttributesProperty = React.JSX.ElementAttributesProperty;
+// 		type ElementChildrenAttribute = React.JSX.ElementChildrenAttribute;
+// 		type IntrinsicElements = React.JSX.IntrinsicElements;
+// 	}
+// }
+
+declare global {
+	type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
+}
