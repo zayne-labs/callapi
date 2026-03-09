88f094891ec6a69980b8d104674c982b93512a6f
Ryan-Zayne <phoenixastro97@gmail.com>
2026-03-09 14:41:21 +0100

    update

--- apps/docs/app/Providers.tsx
+++ apps/docs/app/Providers.tsx
@@ -3,18 +3,15 @@
 import { ProgressProvider } from "@bprogress/next/app";
 import { RootProvider as FumaThemeProvider } from "fumadocs-ui/provider/next";
 import { SonnerToaster } from "@/components/common/Toaster";
-import { useIsDarkMode } from "@/lib/hooks/useIsDarkMode";
 
 function Providers(props: { children: React.ReactNode }) {
 	const { children } = props;
-
-	const isDarkMode = useIsDarkMode();
 
 	return (
 		<FumaThemeProvider>
 			<ProgressProvider
 				height="2px"
-				color={isDarkMode ? "hsl(250, 100%, 80%)" : "hsl(24.6, 95%, 53.1%)"}
+				color="var(--color-fd-primary)"
 				options={{ showSpinner: false }}
 				shallowRouting={true}
 			>
